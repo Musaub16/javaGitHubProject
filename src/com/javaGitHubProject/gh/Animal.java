3054c21428c7a54407e7091ceec172f972d2a595
Darda <dardausa@gmail.com>
2021-04-06 06:01:50 -0500

    First commit

---
+++ src/com/javaGitHubProject/gh/Animal.java
@@ -0,0 +1,18 @@
+package com.javaGitHubProject.gh;
+
+
+
+public class Animal {
+
+	public static void main(String[] args) {
+	Animal animal = new Animal();
+	
+	animal.sound();
+		
+	}
+	
+	public void sound() {
+		System.out.println("animal calss is executed");
+	}
+
+}
